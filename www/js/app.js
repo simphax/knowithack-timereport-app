4cedbc28cb34f00317a46cd9e634926e153b9873
Simon Nilsson <sim.nilsson@gmail.com>
2016-11-18 19:02:42 +0100

    Swipecards

--- www/js/app.js
+++ www/js/app.js
@@ -4,21 +4,71 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
-
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if(window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+
+    });
 })
+.controller('DateSwiper',function($scope) {
+    console.log('CARDS CTRL');
+    
+    $scope.sliderOptions = {
+        loop: false,
+        direction: 'horizontal', //or vertical
+        speed: 500
+    }
+    
+    $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+        console.log('slider init');
+        // data.slider is the instance of Swiper
+        $scope.slider = data.slider;
+    });
+
+    $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+        console.log('Slide change is beginning');
+    });
+
+    $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+        // note: the indexes are 0-based
+        $scope.activeIndex = data.slider.activeIndex;
+        $scope.previousIndex = data.slider.previousIndex;
+    });
+})
+.controller('HoursSwiper',function($scope) {
+    console.log('CARDS CTRL');
+    
+    $scope.sliderOptions = {
+        loop: false,
+        direction: 'vertical',
+        speed: 500
+    }
+    
+    $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+        console.log('slider init');
+        // data.slider is the instance of Swiper
+        $scope.slider = data.slider;
+    });
+
+    $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+        console.log('Slide change is beginning');
+    });
+
+    $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+        // note: the indexes are 0-based
+        $scope.activeIndex = data.slider.activeIndex;
+        $scope.previousIndex = data.slider.previousIndex;
+    });
+});
